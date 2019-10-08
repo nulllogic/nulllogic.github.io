b9839f24f9d5aaeb8a3f483fbf0fc32b57d29f80
Anastasiya Lukyanova <>
2019-10-08 23:26:48 +0300

    v0.1.110:
    - added prismajs;
    - updated home page.

--- src/pages/16blocks/index.js
+++ src/pages/16blocks/index.js
@@ -1,46 +1,52 @@
 import React from "react"
-import { Link } from "gatsby"
+import {Link} from "gatsby"
 
 import Layout from "../../components/16blocks/layout"
 
 const IndexPage = () => (
-  <Layout>
-    <div className="row main-page">
-    
-      <div className="b-intro" id="intro">
-        <div className="xs-16 column">
-          <div className="welcome">
-            <h1 className="title">16blocks framework</h1>
-            <span className="subtitle">Sexy, simple, sleek tool for developers </span>
-            <a href="https://github.com/nulllogic/nulllogic.github.io" className="btn btn-large btn-default">Download
-              16Blocks</a>
+    <Layout>
+      <div className="n-intro" id="intro">
+        <div className="container">
+          <div className="row">
+            <div className="xs-16">
+              <div className="n-title-block">
+                <h1 className="title">16blocks framework</h1>
+                <span className="subtitle">Simple and sleek tool for developers </span>
+                <a href="https://github.com/nulllogic/nulllogic.github.io" className="btn n-btn">Download</a>
+                <Link to="#" className="btn n-btn">View docs</Link>
+              </div>
+            </div>
           </div>
         </div>
       </div>
-    
-      <div className="b-github">
-        <div className="column xs-16">
-          <div className="commit">
-            Latest Commit on Github:
-            &nbsp;&nbsp;
-            <a href="" className="sha"></a>
-            &nbsp;&nbsp;
-            <span className="date"></span>
-            <a id="github-button" href="https://github.com/nulllogic/nulllogic.github.io"
-               className="btn-flat right grey-text text-lighten-5 waves-effect waves-light hide-on-small-only">Github</a>
+      <div className="commit">
+        <div className="container">
+          <div className="row">
+            <div className="xs-16">
+              Latest Commit on Github:
+              &nbsp;&nbsp;
+              <a href="" className="sha"></a>
+              &nbsp;&nbsp;
+              <span className="date"></span>
+              <a id="github-button" href="https://github.com/nulllogic/nulllogic.github.io"
+                 className="btn-flat right grey-text text-lighten-5 waves-effect waves-light hide-on-small-only">Github</a>
+            </div>
           </div>
         </div>
       </div>
-      
-      <div className="row b-content-introduction">
-        <div className="xs-16 column">
-          <p className="greetings">Welcome to 16Block CSS framework, that will give you most of development needs, that
-            can hustle development of websites. It was build in love and with love of many technologies from the
-            internet.</p>
+      <div className="container">
+        <div className="row">
+          <div className="xs-16">
+            <div className="b-content-introduction">
+              <p className="greetings">Welcome to 16Block CSS framework, that will give you most of development needs,
+                that
+                can hustle development of websites. It was build in love and with love of many technologies from the
+                internet.</p>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-  </Layout>
+    </Layout>
 )
 
 export default IndexPage
